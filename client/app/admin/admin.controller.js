893c68aa453dba4eaef3018de010805fd61e9630
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-08-14 12:49:50 -0400

    adding location.url tracking for conditional includes in admin template

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,6 +1,14 @@
 (function () {
 angular.module('notinphillyServerApp')
-  .controller('AdminController', [ '$scope', 'sessionService', function($scope, sessionService) {
+  .controller('AdminController', [ '$scope', '$location', 'sessionService', function($scope, $location, sessionService) {
+
+  	// track location 
+    $scope.url = $location.url();
+
+    $scope.$on('$locationChangeStart', function(event) {
+        $scope.url = $location.url();
+    });
+
     sessionService.checkLoggedin()
                   .then(function() {
                     $scope.isUserAdmin = sessionService.isAdmin();
